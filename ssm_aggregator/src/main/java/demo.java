707a915d79004169da047358acd343d566ed61bf
letehe <letehenevermore@gmail.com>
2019-06-18 22:15:40 +0800

    demo

---
+++ ssm_aggregator/src/main/java/demo.java
@@ -0,0 +1,12 @@
+/**
+ * @author 王波
+ * @version V1.0
+ * @date 2019/6/18 22:12
+ * @discription:
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("=======");
+    }
+}
